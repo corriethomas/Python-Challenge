2003a5a917988bcd41c9f23e38565c1a19329800
Corrie Thomas <corrierthomas@gmail.com>
2021-01-14 15:35:43 -0500

    comments what I want to do after fixing code

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -11,21 +11,12 @@ with open(election_data) as csvfile:
     election_header = next(election_reader)
 
     #Print header to test code
-    # print(election_header)
-
-    #Find the total number of votes
-    # totalcount = []
-
-    # for row in election_reader:
-    #     totalcount.append(1)
-
-    #     vote_sum = sum(totalcount)
-    # print(vote_sum)
+    #print(election_header)
 
     #List the candidates who received votes and count the total votes
     cand_name = []
     total_votes = []
-    
+
     #Add to total number of votes if already listed
     for row in election_reader:
         if row[2] in cand_name:
@@ -36,32 +27,52 @@ with open(election_data) as csvfile:
         
     vote_sum = sum(total_votes)
     print(vote_sum)
-
+  
     #Start dictionary
     cand_dict = {"candidates": [cand_name]}
     print(cand_dict)
     
     #Find how many votes each candidate received
+    Khan_votes = []
+    Correy_votes = []
+    Li_votes = []
+    Otooley_votes = []
+
+    for row in election_reader:
+        if row[2] == "Khan":
+            Khan_votes.append(1)
+        elif row[2] == "Correy":
+            Correy_votes.append(1)
+        elif row[2] == "Li":
+            Li_votes.append(1)
+        elif row[2] == "O'Tooley":
+            Otooley_votes.append(1)
+
+    Khan_sum = sum(Khan_votes)
+    print(Khan_sum)
+
+    #Correy_sum = sum(Correy_votes)    
+    #print(Correy_sum)
     
-    # for row in election_reader:
-    #     if row[2] == "Khan":
-    #         Khan_votes = Khan_votes + 1
-    #     elif row[2] == "Correy":
-    #         Correy_votes == Correy_votes + 1
-    #     elif row[2] == "Li":
-    #         Li_votes == Li_votes + 1
-    #     elif row[2] == "O'Tooley":
-    #         Otooley_votes == Otooley_votes + 1
-    
-    # print(Khan_votes)
-    # print(Correy_votes)
-    # print(Li_votes)
-    # print(Otooley_votes)
+    #Li_sum = sum(Li_votes)
+    #print(Li_sum)
+
+    #Otooley_sum = sum(Otooley_votes)
+    #print(Otooley_sum)
 
 
     #Calculate the percent of votes each candidate won
-    
-    #Calculate the total number of votes each candidate won
+        #Khan_percent = Khan_sum / total_votes
+        #print(Khan_percent)
+
+        #Correy_percent = Correy_sum / total_votes
+        #print(Correy_percent)
+
+        #Li_percent = Li_sum / total_votes
+        #print(Li_percent)
+
+        #Otooley_percent = Otooley_sum / total_votes
+        #print(Otooley_percent)
 
     #Find the winner based on popular votes
 
