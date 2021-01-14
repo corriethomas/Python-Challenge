8bf3fc7b0bf36bd08cd371ff4bc24a4ec52fe318
Corrie Thomas <corrierthomas@gmail.com>
2021-01-14 14:15:53 -0500

    finds total votes

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -22,30 +22,41 @@ with open(election_data) as csvfile:
     #     vote_sum = sum(totalcount)
     # print(vote_sum)
 
-    #List the candidates who received votes
+    #List the candidates who received votes and count the total votes
     cand_name = []
-
+    total_votes = []
+    
     #Add to total number of votes if already listed
     for row in election_reader:
         if row[2] in cand_name:
-            continue
+            total_votes.append(1)
 
         #Add new candidate if not listed, and add to total number of votes    
-        else: cand_name.append(row[2])
+        else: cand_name.append(row[2]) and total_votes.append(1)
         
-    print(cand_name)
+    vote_sum = sum(total_votes)
+    print(vote_sum)
 
-    #Find how many votes each candidate received
-    Khan_votes = []
-    Correy_votes = 0
-    Li_votes = 0
-    Otooley_votes = 0
+    #Start dictionary
+    cand_dict = {"candidates": [cand_name]}
+    print(cand_dict)
     
-    for row in election_reader:
-        if row[2] == 'Khan':
-            Khan_votes.append(1)
-
-    print(Khan_votes)
+    #Find how many votes each candidate received
+    
+    # for row in election_reader:
+    #     if row[2] == "Khan":
+    #         Khan_votes = Khan_votes + 1
+    #     elif row[2] == "Correy":
+    #         Correy_votes == Correy_votes + 1
+    #     elif row[2] == "Li":
+    #         Li_votes == Li_votes + 1
+    #     elif row[2] == "O'Tooley":
+    #         Otooley_votes == Otooley_votes + 1
+    
+    # print(Khan_votes)
+    # print(Correy_votes)
+    # print(Li_votes)
+    # print(Otooley_votes)
 
 
     #Calculate the percent of votes each candidate won
